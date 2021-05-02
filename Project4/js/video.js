33d4d17d5fc49c9e50b27976abe4b9f0fbb0e817
myklquinn <80681768+myklquinn@users.noreply.github.com>
2021-05-03 01:55:48 +0200

    cleanup

--- Project4/js/video.js
+++
@@ -1,6 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const video = urlParams.get('video')
-
-document.querySelector("video").src = video
-document.querySelector("video source").src = video
